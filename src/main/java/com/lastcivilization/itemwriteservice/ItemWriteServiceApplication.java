e9b559b55bbc43f83c53cc1ea9688a7083f4b299
mrrevo55 <kwolny31@gmail.com>
2022-09-23 15:44:12 +0200

    Initial commit

---
+++ src/main/java/com/lastcivilization/itemwriteservice/ItemWriteServiceApplication.java
@@ -0,0 +1,13 @@
+package com.lastcivilization.itemwriteservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemWriteServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItemWriteServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lastcivilization/itemwriteservice/ItemWriteServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lastcivilization.itemwriteservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemWriteServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
